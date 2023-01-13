003eacf1b4020c919c1b2c56515cb5f3808a508b
quarkjoy <isabellenoelgreen@gmail.com>
2023-01-12 22:01:37 -0800

    WELCOME

---
+++ src/main/java/net/quarkjoy/spidereyeore/SpiderEyeOre.java
@@ -0,0 +1,14 @@
+package net.quarkjoy.spidereyeore;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpiderEyeOre implements ModInitializer {
+    public static final String MOD_ID = "spidereyeore";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/quarkjoy/spidereyeore/SpiderEyeOreClient.java
@@ -0,0 +1,10 @@
+package net.quarkjoy.spidereyeore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiderEyeOreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
